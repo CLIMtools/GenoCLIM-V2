df90272434d4eada911836eb64242dbaccd6b507
Arabidopsis CLIMtools <43958844+CLIMtools@users.noreply.github.com>
2022-04-18 11:53:40 -0400

    update

--- server.R
+++ server.R
@@ -6,31 +6,48 @@ library(readr)
 shinyServer(function(input, output) {
  
 descriptiondataset <-read_csv("data/datadescription.csv")
+
+
       
  output$a <- DT::renderDataTable(descriptiondataset, filter = 'top', options = list(
         pageLength = 100, autoWidth = TRUE))
   
   data <- read_csv("data/merged1.csv")
+  LOCUS_ID <- sort(unique(data$LOCUS_ID))
   
+# Filter data based on selections
+
+  #Unique LOCUS_ID dropdown
   # Filter data based on selections
-  output$table <-DT::renderDataTable(datatable(data, options = list( searchDelay = 500,
-    initComplete = JS(
-      "function(settings, json) {",
-      "$(this.api().table().header()).css({'background-color': '#0E7DE3', 'color': '#fff'});",
-      "}")
-  ),
-                                                rownames = FALSE,
-                                                selection = 'none'
-) %>%
-                                        formatStyle('score_AMM',backgroundColor = 'orange',  color = styleInterval(c(4, 6), c('black', 'blue', 'red')),fontWeight = 'bold',
-                                                    background = styleColorBar(data$score_AMM, 'lightblue'),
-                                                    backgroundSize = '98% 88%',
-                                                    backgroundRepeat = 'no-repeat',
-                                                    backgroundPosition = 'center')
-                                       
-  )
-      })
+  output$results <- DT::renderDataTable({
+    filtered <- 
+      data %>%
+      filter(LOCUS_ID == input$LOCUS_ID)
+    filtered
+  }, filter ='top',
+  options = list( autoWidth = TRUE,dom = 't', columnDefs = 
+                   list(list(className = 'dt-center', 
+                             targets = "_all"))), callback = JS("
+var tips = ['Chromosome', 'Position', 'Reference allele (col-0)',
+          'Alternative allele', 'Minor allele frequency', 'Minor allele count', 
+          'score (-log(p-value)) using a mixed model approach correcting for population structure. The higher the value is, the stronger the association is between genetic and environmental variation.',
+          'score (-log(p-value)) using a linear model approach not correcting for population structure. The higher the value is, the stronger the association is between genetic and environmental variation.', 
+          'q-values using a mixed model approach correcting for population structure.' ,
+          'q-values using a linear model approach not correcting for population structure', 'Estimate local False Discovery Rate (FDR) using a mixed model approach correcting for population structure',
+          'Estimate local False Discovery Rate (FDR) using a linear model approach not correcting for population structure',
+          'Spearman’s rank correlation coeﬃcients between individual climate varriables and individual transcript abundance values. The stronger the association between climate and transcript variation, the closer the Pearson correlation coeﬃcient, rs, will be to either + 1 or − 1',
+          'Correlation coefficient obtained using the SNPfold program. The closer this correlation coefficient is to 1, the less likely it is that the RNA structure is changed by the SNP. See text on the sidebar for a more detailed explanation.',
+          'The fixation index (FST) is a measure of population differentiation due to genetic structure.',
+          'Tajima D compares an observed nucleotide diversity against the expected diversity under the assumption that all polymorphisms are selectively neutral and constant population size.',
+          'Nucleotide diversity (PI) calculated using a sliding window of 1 kb is the average pairwise difference between all possible pairs of individuals in the population',
+          'Each locus in Arabidopsis is assigned a unique ID, termed the AGI locus code (AGI, Arabidopsis Genome Initiative). This ID consists of the prefix AT, followed by the chromosome identifier, a G (gene), and then a unique 5 digit number',
+          'Current symbol for the gene locus (following (TAIR10.52)','Current annotation for the gene locus (following (TAIR10.52)','SNP effect prediction according to SnpEff using the latest TAIR10.52 release. See http://snpeff.sourceforge.net/VCFannotationformat_v1.0.pdf for definitions',
+          'Source from which the environmental associated with the SNP was obtained from. See the tab with the description of climate variables for more information' ,'Description of the environmental associated with the SNP was obtained from. See the tab with the description of climate variables for more information'
+          ],
+    header = table.columns().header();
+for (var i = 0; i < tips.length; i++) {
+  $(header[i]).attr('title', tips[i]);
+}
+"),
+  rownames= FALSE)})
   
-  
-
-
--- ui.R
+++ ui.R
@@ -7,6 +7,12 @@ library(shinycssloaders)
 library(shinyjqui)
 library(shinyjs)
 library(markdown)
+library(spsComps)
+library(dplyr, warn.conflicts = FALSE)
+library(shinyWidgets)
+library(readr)
+data <- read_csv("data/merged1.csv")
+LOCUS_ID <- sort(unique(data$LOCUS_ID))
 
 shinyUI(fluidPage(
   # Add Javascript
@@ -27,18 +33,21 @@ shinyUI(fluidPage(
                            style="float:left"/>','<p style="color:blue">                    Find Your Gene’s Environmental Association </p>' ))
   ),
   
-  theme = shinytheme("cerulean") , 
-  
+  theme = shinytheme("journal") , 
   
   tabsetPanel(              
-    tabPanel(type = "pills", title = "Find your gene",(jqui_draggable(sidebarPanel(
-      wellPanel(a(h4('SNPfold CC:'),  h6('The SNPfold algorithm (Halvorsen et al., 2010) considers the ensemble of structures predicted by the RNA partition functions of RNAfold (Bindewald & Shapiro, 2006) for each reference and alternative sequence and quantifies structural differences between these ensembles by calculating a Pearson correlation coefficient on the base-pairing probabilities between the two sequences. The closer this correlation coefficient is to 1, the less likely it is that the RNA structure is changed by the SNP. The creators of SNPfold note (Corley et al., 2015) that for genome-wide prediction, the bottom 5% of the correlation coefficient values (corresponding in this CLIMtools dataset to a correlation coefficient of 0.445) are most likely to be riboSNitches and the top 5% of correlation coefficient values (corresponding in this CLIMtools dataset to a correlation coefficient of 0.99) are most likely to be non-riboSNitches.'))),
-                  wellPanel(a(h4('Please cite us in any publication that utilizes information from Arabidopsis CLIMtools:'),  h6('-Ferrero‑Serrano,Á, Sylvia, MM, Forstmeier, PC, Olson, AJ, Ware, D,Bevilacqua, PC & Assmann, SM (2021). Experimental demonstration and pan‑structurome prediction of climate‑associated riboSNitches in Arabidopsis. Under review in Genome Biology.' ), h6('-Ferrero-Serrano, Á & Assmann SM. Phenotypic and genome-wide association with the local environment of Arabidopsis. Nature Ecology & Evolution. doi: 10.1038/s41559-018-0754-5 (2019)' ))),
-      
-      #    wellPanel(
-      #      uiOutput("datasets")
-      #    ),
-      #uiOutput("ui_Manage")
+    tabPanel(type = "pills", title = "Find your gene",(sidebarPanel(
+      wellPanel(pickerInput("LOCUS_ID", "Type your locus/loci of interest using the AGI locus code in the search box to explore the association between its genetic variation with any geoclimatic variable (i.e. AT5G35840)", choices = LOCUS_ID, selected = "AT5G35840",  options = list(`actions-box` = TRUE, `live-search` = TRUE), multiple = TRUE),style = "background: #c0e6ea" ),
+      wellPanel(a(h4('SNPfold Correlation Coefficient (SNPfold CC)'),  h6(style="text-align: justify;",'The SNPfold algorithm (Halvorsen et al., 2010) considers the ensemble of structures predicted by the RNA partition functions of RNAfold (Bindewald & Shapiro, 2006) for each reference and alternative sequence and quantifies structural differences between these ensembles by calculating a Pearson correlation coefficient on the base-pairing probabilities between the two sequences. The closer this correlation coefficient is to 1, the less likely it is that the RNA structure is changed by the SNP. The creators of SNPfold note (Corley et al., 2015) that for genome-wide prediction, the bottom 5% of the correlation coefficient values (corresponding in this CLIMtools dataset to a correlation coefficient of 0.445) are most likely to be riboSNitches and the top 5% of correlation coefficient values (corresponding in this CLIMtools dataset to a correlation coefficient of 0.99) are most likely to be non-riboSNitches.'), 
+                  h6('-Halvorsen M, Martin JS, Broadaway S, Laederach A. Disease‐associated mutations that alter the RNA structural ensemble. PLoS Genet. 2010;6:e1001074.'),
+                  h6('-Bindewald, E, & Shapiro, BA. RNA secondary structure prediction from sequence alignments using a network of k-nearest neighbor classifiers. Rna. 2006;12:342-352.'))),
+                  wellPanel(a(h4('Please cite us in any publication that utilizes information from Arabidopsis CLIMtools:'),  
+                  h6('-Ferrero‑Serrano, Á, Sylvia, MM, Forstmeier, PC, Olson, AJ, Ware, D,Bevilacqua, PC & Assmann, SM. Experimental demonstration and pan‑structurome prediction of climate‑associated riboSNitches in Arabidopsis. Genome Biology. DOI: 10.1186/s13059‐022‐02656‐4.' ), h6('-Ferrero-Serrano, Á & Assmann SM. Phenotypic and genome-wide association with the local environment of Arabidopsis. Nature Ecology & Evolution. DOI: 10.1038/s41559-018-0754-5 (2019)' ))),
+   
+  #    wellPanel(
+  #      uiOutput("datasets")
+  #    ),
+  #uiOutput("ui_Manage")
       uiOutput("ui_All"),
       width=3, wellPanel(tags$a(img(src='genoclimwarning.png', h3("Considerations before using this tool"), height="120px"),href="myfile.pdf"),align="center"), wellPanel(tags$a(img(src='FDR.png', h3("Explore FDR of any ExG association"), height="120px"),href="myfile.pdf"),align="center"), wellPanel(a("Tweets by @ClimTools", class="twitter-timeline"
                                                                                                                                                                                                                                                                                                               , href = "https://twitter.com/ClimTools"), style = "overflow-y:scroll; max-height: 1000px"
@@ -48,27 +57,22 @@ shinyUI(fluidPage(
       ), href = "https://github.com/CLIMtools/GenoCLIM"))
     )
       
-      ###################################################
-            
-                                                                       
-                                                                       
-                                                                       ###################################################
-    )                 
-    
+  ###################################################
+  ###################################################
     ),
     
-                             ###################################################
-          
-    mainPanel(  
-        ###add code to get rid of error messages on the app.   
+  ###################################################
+  mainPanel(  
+  ###add code to get rid of error messages on the app.   
         tags$style(type="text/css",
                    ".shiny-output-error { visibility: hidden; }",
                    ".shiny-output-error:before { visibility: hidden; }"
         ),
              
   # Create a new row for the table.
-  fixedRow(column(6,h4("Type your locus of interest in the search box to explore the association between its genetic variation with any geoclimatic variable (i.e. AT5G35840 or PHYC)"),
-    withSpinner(DT::dataTableOutput("table"))
+  
+  fixedRow(column(6,h4("Mouse-over column names and wait for the pop-up for a more detailed description of variables"),
+    withSpinner(DT::dataTableOutput("results"))
     
   )))), 
   tabPanel(title = "Description of climate variables",  mainPanel(fixedRow(
@@ -76,25 +80,18 @@ shinyUI(fluidPage(
     withSpinner(DT::dataTableOutput("a"))
   ))),
   
-  tabPanel(title = "About",  mainPanel(h1(div('About GenoCLIM', style = "color:blue")), 
-                                       h3(div('GenoCLIM is an SHINY component of CLIMtools, that provides an intuitive tool to explore the environmental variation associated to any gene or variant of interest in Arabidopis.', style = "color:grey")),
+  tabPanel(title = "About",  mainPanel(h1(div('About GenoCLIM V2.0', style = "color:blue")), 
+                                       h3(div('GenoCLIM V2.0 is an SHINY component of CLIMtools V2.0, that provides an intuitive tool to explore the environmental variation associated to any gene or variant of interest in Arabidopis.', style = "color:grey")),
                                        h3(div('GenoCLIM allows the user to input the locus ID, genetic position or keyword within a particular locus description to explore its association with any oif the multiple environmental variables available from CLIMtools ', style = "color:grey")),
-                                       h3(''),
-                                       
-                                       
-                                       div(tags$a(img(src='shiny.png',  height="100px"),href="https://shiny.rstudio.com/"),
-                                           tags$a(img(src='rstudio.png',  height="100px"),href="https://www.rstudio.com/"),
-                                           tags$a(img(src='1001.png',  height="100px"),href="http://1001genomes.org/"),  align="middle", style="text-align: center;"),
+                                       h3(div('Code and data for CLIMtools V2.0 have been uploaded to Dryad and Zenodo https://datadryad.org/stash/dataset/doi:10.5061/dryad.mw6m905zj  ', style = "color:grey")),
                                        
                                        h3(''),
                                        h3(''),
                                        tags$a(div(img(src='climtools.png',  align="middle"), style="text-align: center;"), href="http://www.personal.psu.edu/sma3/CLIMtools.html"),
                                        tags$a(div(img(src='climtools logo.png',  align="middle"), style="text-align: center;"), href="http://www.personal.psu.edu/sma3/CLIMtools.html"),
+                                       tags$a(div(img(src='Gramene.jpg',  align="middle"), style="text-align: center;"), href="https://www.gramene.org/"),
+                                       
                                        
                                        h3(''),
-                                       tags$a(div(img(src='assmann_lab.png',  align="middle"), style="text-align: center;"), href="http://www.personal.psu.edu/sma3/index.html")
-                                       
   )))
-  
 ))
-           
